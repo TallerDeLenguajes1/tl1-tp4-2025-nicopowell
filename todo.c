8463dc74515b7e91a4a2d2214670e18370cfc5e9
nicopowell <nicopowell30@gmail.com>
2025-04-21 21:21:37 -0300

    se termino el tp

--- todo.c
+++ todo.c
@@ -71,6 +71,86 @@ void EliminarLista(Nodo *Start){
     }
 }
 
+void marcarCompletada(Nodo **Pendientes, Nodo **Completadas, int seleccion){
+    Nodo *actual = *Pendientes;
+    Nodo *anterior = NULL;
+    int i = 1;
+
+    while (actual != NULL && i < seleccion) {
+        anterior = actual;
+        actual = actual->Siguiente;
+        i++;
+    }
+
+    if (anterior == NULL) {
+        *Pendientes = actual->Siguiente;
+    } else {
+        anterior->Siguiente = actual->Siguiente;
+    }
+
+    actual->Siguiente = NULL;
+
+    insertarNodo(Completadas, actual);
+}
+
+void buscarTareaPorID(Nodo *Pendientes, Nodo *Realizadas, int idBuscado){
+    Nodo *aux;
+
+    printf("\n=========== Búsqueda por ID ===========\n");
+
+    // Buscar en pendientes
+    aux = Pendientes;
+    while (aux) {
+        if (aux->T.TareaID == idBuscado) {
+            printf("-> [Pendiente]\n\tID: %d\n\tTarea: %s\n\tDuracion: %d\n",
+                   aux->T.TareaID, aux->T.Descripcion, aux->T.Duracion);
+            return;
+        }
+        aux = aux->Siguiente;
+    }
+
+    // Buscar en realizadas
+    aux = Realizadas;
+    while (aux) {
+        if (aux->T.TareaID == idBuscado) {
+            printf("-> [Realizada]\n\tID: %d\n\tTarea: %s\n\tDuracion: %d\n",
+                   aux->T.TareaID, aux->T.Descripcion, aux->T.Duracion);
+            return;
+        }
+        aux = aux->Siguiente;
+    }
+    printf("No se encontró ninguna tarea con el ID %d.\n", idBuscado);
+}
+
+void buscarTareaPorPalabra(Nodo *Pendientes, Nodo *Realizadas, char *clave){
+    Nodo *aux;
+
+    printf("\n=========== Búsqueda por palabra clave ===========\n");
+
+    // Buscar en pendientes
+    aux = Pendientes;
+    while (aux) {
+        if (strstr(aux->T.Descripcion, clave)) {
+            printf("-> [Pendiente]\n\tID: %d\n\tTarea: %s\n\tDuracion: %d\n",
+                   aux->T.TareaID, aux->T.Descripcion, aux->T.Duracion);
+        }
+        aux = aux->Siguiente;
+    }
+
+    // Buscar en realizadas
+    aux = Realizadas;
+    while (aux) {
+        if (strstr(aux->T.Descripcion, clave)) {
+            printf("-> [Realizada]\n\tID: %d\n\tTarea: %s\n\tDuracion: %d\n",
+                   aux->T.TareaID, aux->T.Descripcion, aux->T.Duracion);
+            return;
+        }
+        aux = aux->Siguiente;
+    }
+
+    printf("No se encontró ninguna tarea que contenga la palabra \"%s\".\n", clave);
+}
+
 int aleatorio(int min, int max) {
     return min + rand() % (max + 1 - min);
 }
@@ -107,16 +187,57 @@ int main(){
         
     } while (ingresarOtra);
 
-    cantidadPendientes = cantidadNodos(TareasPendientes);
-
+    
     do {
+        cantidadPendientes = cantidadNodos(TareasPendientes);
         printf("\n============================================================");
         listarTareas(TareasPendientes, 0);
         printf("\n0. Ninguna");
         printf("\nIngrese que tarea desea marcar como completada: ");
         scanf("%d", &seleccion);
+        if (seleccion > 0 && seleccion <= cantidadPendientes){
+            marcarCompletada(&TareasPendientes, &TareasRealizadas, seleccion);
+        }
     } while (seleccion != 0);
     
+    printf("\n===================== Pendientes ==========================");
+    listarTareas(TareasPendientes, 1);
+
+    printf("\n===================== Realizadas ==========================");
+    listarTareas(TareasRealizadas, 1);
+
+    int opcionBusqueda;
+    int idBuscado;
+    char palabraClave[100];
+
+    do {
+        printf("\n=============== MENU DE BUSQUEDA ===============");
+        printf("\n1. Buscar tarea por ID");
+        printf("\n2. Buscar tarea por palabra clave");
+        printf("\n0. Salir");
+        printf("\nSeleccione una opcion: ");
+        scanf("%d", &opcionBusqueda);
+        fflush(stdin);
+
+        switch(opcionBusqueda) {
+            case 1:
+                printf("Ingrese el ID de la tarea: ");
+                scanf("%d", &idBuscado);
+                buscarTareaPorID(TareasPendientes, TareasRealizadas, idBuscado);
+                break;
+            case 2:
+                printf("Ingrese la palabra clave a buscar: ");
+                gets(palabraClave);
+                buscarTareaPorPalabra(TareasPendientes, TareasRealizadas, palabraClave);
+                break;
+            case 0:
+                printf("Saliendo del menú de búsqueda...\n");
+                break;
+            default:
+                printf("Opción inválida. Intente nuevamente.\n");
+        }
+    } while(opcionBusqueda != 0);
+
     EliminarLista(TareasPendientes);
     EliminarLista(TareasRealizadas);
     free(Buff);
